d446111994c0affb3d6e19517845c441eeb5dc1f
mrcorreoweb <145986471+mrcorreoweb@users.noreply.github.com>
2024-10-06 00:14:08 +0200

    Update install.sh
    removed the update of fish

--- install.sh
+++ install.sh
@@ -4,10 +4,6 @@ set -e  # Exit immediately if a command exits with a non-zero status.
 set -x  # Print commands and their arguments as they are executed.
 
 # Fish is pre-installed in Codespaces, so we don't need to install it
-# Instead, let's just make sure it's up to date
-apt-get update
-apt-get upgrade -y fish
-
 # Create Fish config directory if it doesn't exist
 mkdir -p ~/.config/fish
 
